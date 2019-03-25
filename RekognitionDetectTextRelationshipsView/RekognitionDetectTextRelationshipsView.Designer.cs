3d7e2676b44aecbbf9ef215af9775ab7ee6115f1
h.shigemoto <h.shigemoto@pureatio.com>
2019-03-25 20:30:13 +0900

    add Rekognition detect-text Relationships.

---
+++ RekognitionDetectTextRelationshipsView/RekognitionDetectTextRelationshipsView.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace RekognitionDetectTextRelationshipsView
+{
+    partial class RekognitionDetectTextRelationshipsView
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.relationTreeView = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // relationTreeView
+            // 
+            this.relationTreeView.AllowDrop = true;
+            this.relationTreeView.Location = new System.Drawing.Point(12, 12);
+            this.relationTreeView.Name = "relationTreeView";
+            this.relationTreeView.Size = new System.Drawing.Size(483, 408);
+            this.relationTreeView.TabIndex = 0;
+            this.relationTreeView.DragDrop += new System.Windows.Forms.DragEventHandler(this.relationTreeView_DragDrop);
+            this.relationTreeView.DragEnter += new System.Windows.Forms.DragEventHandler(this.relationTreeView_DragEnter);
+            // 
+            // RekognitionDetectTextRelationshipsView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 432);
+            this.Controls.Add(this.relationTreeView);
+            this.Name = "RekognitionDetectTextRelationshipsView";
+            this.Text = "RekognitionDetectTextRelationshipsView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView relationTreeView;
+    }
+}
+
---
+++ RekognitionDetectTextRelationshipsView/RekognitionDetectTextRelationshipsView.cs
@@ -0,0 +1,82 @@
+﻿using RekognitionExtensions;
+using RekognitionExtensions.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RekognitionDetectTextRelationshipsView
+{
+    public partial class RekognitionDetectTextRelationshipsView : Form
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public RekognitionDetectTextRelationshipsView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ドラッグ開始イベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void relationTreeView_DragEnter(object sender, DragEventArgs e)
+        {
+            // ファイルの場合、エフェクトをAllに変更する
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// ドロップイベント
+        /// </summary>
+        /// <param name="sender">イベント発生元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void relationTreeView_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドロップされたファイル情報を取得する。
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[];
+            string filePath = files[0];
+
+            Dictionary<ValueDto, List<ValueDto>> textRelationships = Analyzer.AnalyzeDetectText(filePath);
+            this.relationTreeView.Nodes.Clear();
+
+            // TreeViewを構成する。
+            this.SetTextRelationships(textRelationships);
+        }
+
+        /// <summary>
+        /// テキストの紐付けをTreeViewに設定
+        /// </summary>
+        /// <param name="textRelationships">テキストの紐付け</param>
+        private void SetTextRelationships(Dictionary<ValueDto, List<ValueDto>> textRelationships)
+        {
+            foreach (KeyValuePair<ValueDto, List<ValueDto>> linePair in textRelationships)
+            {
+                // LINEの情報を設定する。
+                TreeNode lineNode = new TreeNode(string.Format("{0}({1})", linePair.Key.Text, linePair.Key.ID));
+
+                foreach (ValueDto wordDto in linePair.Value)
+                {
+                    TreeNode wordNode = new TreeNode(string.Format("{0}({1})", wordDto.Text, wordDto.ID));
+                    lineNode.Nodes.Add(wordNode);
+                }
+
+                this.relationTreeView.Nodes.Add(lineNode);
+            }
+        }
+    }
+}
